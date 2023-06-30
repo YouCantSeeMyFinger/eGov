a384bf6e603f33d4744d346c1950031540c681ef
gyoon <xmas6904201@gmail.com>
2023-06-30 19:04:55 +0900

    Feat(main.js 함수 추가): JS를 이용하여 게시판 출력
    main.js에 함수 추가

--- SpringMVC02/src/main/webapp/resources/js/home.js
+++ SpringMVC02/src/main/webapp/resources/js/home.js
@@ -15,5 +15,32 @@ function loadList() {
 }
 
 function makeView(data) {
-	alert(data);
+	// table 시작
+	let listHtml = "<table class = 'table table-bordered'>"
+
+	// thead 추가
+	listHtml += "<tr>"
+	listHtml += "<td>번호</td>"
+	listHtml += "<td>제목</td>"
+	listHtml += "<td>작성자</td>"
+	listHtml += "<td>작성일</td>"
+	listHtml += "<td>조회수</td>"
+	listHtml += "</tr>"
+
+	// tbody 함수
+	// 반복문
+	$.each(data, function(index, object) {
+		listHtml += "<tr>"
+		listHtml += "<td>" + object.idx + "</td>"
+		listHtml += "<td>" + object.title + "</td>"
+		listHtml += "<td>" + object.writer + "</td>"
+		listHtml += "<td>" + object.indate + "</td>"
+		listHtml += "<td>" + object.count + "</td>"
+		listHtml += "</tr>"
+	});
+
+	// table 끝
+	listHtml += "</table>"
+
+	$("#view").html(listHtml);
 }
