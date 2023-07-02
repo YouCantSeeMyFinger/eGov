50d560194d2613bac76eaa7be4459ca18f8c5874
gyoon <xmas6904201@gmail.com>
2023-07-02 11:26:02 +0900

    feat(Js): JavaScript , Ajax통신 [#9]

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.board.entity.Board;
 import kr.board.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
@@ -28,6 +31,18 @@ public class BoardController {
 	public List<Board> boardList() {
 		List<Board> list = this.boardMapper.getLists();
 		return list;
+	}
+
+	/**
+	 * Js에서 받은 데이터를 Board의 필드에 맞게 바인딩 해주는 작업을 Spring이 알아서 해준다.
+	 * @param vo
+	 */
+
+	@PostMapping("/boardInsert.do")
+	@ResponseBody
+	public void boardInsert(Board vo) {
+		log.info("board : {}", vo);
+		this.boardMapper.boardInsert(vo);
 	}
 
 }
--- SpringMVC02/src/main/webapp/resources/js/home.js
+++ SpringMVC02/src/main/webapp/resources/js/home.js
@@ -1,48 +1,108 @@
 $(() => {
-    loadList();
+	loadList();
 });
 
 function loadList() {
-    $.ajax({
-        url: "boardList.do",
-        dataType: "json",
-        type: "get",
-        // CallBackFunction
-        success: makeView,
-        error: function () {
-            alert("error");
-        }
-    });
+	$.ajax({
+		url: "boardList.do",
+		dataType: "json",
+		type: "get",
+		success: function(data) {
+			makeView(data);
+			goList()
+		},
+		error: error
+	});
 }
 
 function makeView(data) {
 
-    let boardHtml = [];
-    const $view = $("#view");
-    boardHtml.push("<table class='table table-bordered'>");
-    boardHtml.push("<thead>");
-    boardHtml.push("<tr>");
-    boardHtml.push("<td>번호</td>");
-    boardHtml.push("<td>제목</td>");
-    boardHtml.push("<td>작성자</td>");
-    boardHtml.push("<td>작성일</td>");
-    boardHtml.push("<td>조회수</td>");
-    boardHtml.push("</tr>");
-    boardHtml.push("</thead>");
+	let boardHtml = [];
+	const $view = $("#view");
+	boardHtml.push("<table class='table table-bordered'>");
 
-    boardHtml.push("<tbody>");
-    $.each(data, function (index, object) {
-        boardHtml.push("<tr>");
-        boardHtml.push("<td>" + object.idx + "</td>");
-        boardHtml.push("<td>" + object.title + "</td>");
-        boardHtml.push("<td>" + object.writer + "</td>");
-        boardHtml.push("<td>" + object.indate + "</td>");
-        boardHtml.push("<td>" + object.count + "</td>");
-        boardHtml.push("</tr>");
-    });
-    boardHtml.push("</tbody>");
+	boardHtml.push("<thead>");
+	boardHtml.push("<tr>");
+	boardHtml.push("<td>번호</td>");
+	boardHtml.push("<td>제목</td>");
+	boardHtml.push("<td>작성자</td>");
+	boardHtml.push("<td>작성일</td>");
+	boardHtml.push("<td>조회수</td>");
+	boardHtml.push("</tr>");
+	boardHtml.push("</thead>");
 
-    boardHtml.push("</table>")
+	boardHtml.push("<tbody>");
+	$.each(data, function(index, object) {
+		boardHtml.push("<tr>");
+		boardHtml.push("<td>" + object.idx + "</td>");
+		boardHtml.push(`<td><a href="javascript:showBoardContext(${object.idx})">${object.title}</a></td>`);
+		boardHtml.push("<td>" + object.writer + "</td>");
+		boardHtml.push("<td>" + object.indate + "</td>");
+		boardHtml.push("<td>" + object.count + "</td>");
+		boardHtml.push("</tr>");
 
-    $view.html(boardHtml.join(" "));
+		boardHtml.push(`<tr id="b${object.idx}" style="display:none">`);
+		boardHtml.push("<td>내용</td>");
+		boardHtml.push("<td colspan='4'>");
+		boardHtml.push(`<textarea row="7" class="form-control" readonly>${object.content}</textarea>`);
+		boardHtml.push("</td>")
+		boardHtml.push("</tr>");
+
+	});
+	boardHtml.push("</tbody>");
+
+	boardHtml.push("<tr>");
+	boardHtml.push("<td colspan='5'>");
+	boardHtml.push("<button type='button' class='btn btn-primary btn-sm' value='글 쓰기' onclick='goForm()'>글 쓰기</button>");
+	boardHtml.push("</td>");
+	boardHtml.push("</tr>");
+	boardHtml.push("</table>");
+	$view.html(boardHtml.join(" "));
+
+
+
+} // End makeView ()
+
+function goForm() {
+	$("#view").css("display", "none");
+	$("#boardForm").css("display", "block");
+}
+
+function goList() {
+	$("#view").css("display", "block");
+	$("#boardForm").css("display", "none");
+}
+
+function boardInsert() {
+	let formData = $("#tableForm").serialize();
+
+	$.ajax({
+		url: "boardInsert.do",
+		type: "post",
+		data: formData,
+		success: loadList,
+		error: error
+	});
+
+	$("#title").val("");
+	$("#content").val("");
+	$("#writer").val("");
+}
+
+function error() {
+	alert("Ajax 통신 에러");
+}
+
+function showBoardContext(idx) {
+
+	let $targetId = $(`#b${idx}`);
+	let displayValue = $targetId.css("display");
+
+	if (displayValue === "none") {
+		displayValue = "table-row";
+		$targetId.css("display", displayValue);
+	} else {
+		displayValue = "none";
+		$targetId.css("display", displayValue);
+	}
 }
